3d9eada621407017edb55956dc060da2218ebfa1
Hoobbaboobba <xopoxopox@gmail.com>
2024-01-03 17:36:21 +0300

    update auth.js

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,9 +4,5 @@ import { redirect } from "next/navigation";
 export const currentUser = async () => {
   const session = await auth();
 
-  if (!session) {
-    redirect("/auth/login");
-  }
-
-  return session.user;
+  return session?.user;
 };
