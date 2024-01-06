68b22665fca95144c9b32327c8db296ce830400d
Hoobbaboobba <xopoxopox@gmail.com>
2024-01-06 20:11:11 +0300

    game loading

---
+++ app/(protected)/play/[gameId]/loading.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton className="rounded-2xl w-[278px] h-[200px]" />
+      <Skeleton className="w-[320px] h-[320px] rounded-xl" />
+    </div>
+  );
+}
